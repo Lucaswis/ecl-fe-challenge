b63fa4b2ccb878917317dbadd366bf0bf2a81dd2
Lucas Wisgikl <lucaswisgikl6@gmail.com>
2026-08-07 11:26:29 -0300

    feat(assets): prepend created assets and filter deleted ids in mergeAssets

--- ecl-fe/src/lib/assets/local-store.ts
+++ ecl-fe/src/lib/assets/local-store.ts
@@ -1,9 +1,13 @@
 import type { AssetWithSeverity, LocalAsset } from "./types"
 
+function asAssetWithSeverity(local: LocalAsset): AssetWithSeverity {
+  return { ...local, highestSeverity: null, vulnerabilityCount: local.vulnerabilities.length }
+}
+
 export function mergeAssets(
   base: AssetWithSeverity[],
   created: LocalAsset[],
   deletedIds: ReadonlySet<string>
 ): AssetWithSeverity[] {
-  return base
+  return [...created.map(asAssetWithSeverity), ...base].filter((asset) => !deletedIds.has(asset.id))
 }
