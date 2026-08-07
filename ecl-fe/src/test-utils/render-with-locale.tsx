a44c4e4a22c017b0914f669d2818c55ea7e7dd78
Lucas Wisgikl <lucaswisgikl6@gmail.com>
2026-08-07 11:35:48 -0300

    test(test-utils): wrap renderWithLocale in AssetStoreProvider

--- ecl-fe/src/test-utils/render-with-locale.tsx
+++ ecl-fe/src/test-utils/render-with-locale.tsx
@@ -1,12 +1,17 @@
 import type { ReactElement } from "react"
 import { render } from "@testing-library/react"
 
+import { AssetStoreProvider } from "@/components/asset-store-context"
 import { LocaleProvider } from "@/lib/i18n/locale-context"
 import { DEFAULT_LOCALE } from "@/lib/i18n/types"
 import type { Locale } from "@/lib/i18n/types"
 
 export function renderWithLocale(ui: ReactElement, locale: Locale = DEFAULT_LOCALE) {
   return render(ui, {
-    wrapper: ({ children }) => <LocaleProvider locale={locale}>{children}</LocaleProvider>,
+    wrapper: ({ children }) => (
+      <LocaleProvider locale={locale}>
+        <AssetStoreProvider>{children}</AssetStoreProvider>
+      </LocaleProvider>
+    ),
   })
 }
